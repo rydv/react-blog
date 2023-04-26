298fe174574ed1ac5f1cb55da8488db23b51596d
Sanjay <sanjayrydv@gmail.com>
2023-04-27 01:10:36 +0530

    added register api

--- api/index.js
+++ api/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
-
-const dotenv= require("dotenv")
+const mongoose = require("mongoose");
+const dotenv= require("dotenv");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL,
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(console.log("Connected to Url")).catch((err)=> console.log(err));
+mongoose
+    .connect(process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log("Connected to Url"))
+    .catch((err)=> console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5002", () => {
     console.log("Backend is running...")
