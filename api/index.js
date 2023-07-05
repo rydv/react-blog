62315c039e7acdd5e780819f99defceac6e8f07e
Sanjay <sanjayrydv@gmail.com>
2023-07-06 02:10:25 +0530

    added cors

--- api/index.js
+++ api/index.js
@@ -8,9 +8,11 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors")
 
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 app.use("/images", express.static(path.join(__dirname,"/images")))
 
 mongoose
